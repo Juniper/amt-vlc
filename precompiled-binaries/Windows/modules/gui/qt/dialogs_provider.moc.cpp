9ed77540b816a870c2fe07e960085b56f36050c1
Natalie Landsberg <nlandsberg@juniper.net>
2018-08-10 10:27:00 -0700

    Compiled Windows binary, time to test

---
+++ precompiled-binaries/Windows/modules/gui/qt/dialogs_provider.moc.cpp
@@ -0,0 +1,345 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'dialogs_provider.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../modules/gui/qt/dialogs_provider.hpp"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'dialogs_provider.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_DialogsProvider_t {
+    QByteArrayData data[52];
+    char stringdata0[716];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_DialogsProvider_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_DialogsProvider_t qt_meta_stringdata_DialogsProvider = {
+    {
+QT_MOC_LITERAL(0, 0, 15), // "DialogsProvider"
+QT_MOC_LITERAL(1, 16, 18), // "toolBarConfUpdated"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 18), // "releaseMouseEvents"
+QT_MOC_LITERAL(4, 55, 14), // "playlistDialog"
+QT_MOC_LITERAL(5, 70, 15), // "bookmarksDialog"
+QT_MOC_LITERAL(6, 86, 15), // "mediaInfoDialog"
+QT_MOC_LITERAL(7, 102, 16), // "mediaCodecDialog"
+QT_MOC_LITERAL(8, 119, 11), // "prefsDialog"
+QT_MOC_LITERAL(9, 131, 14), // "extendedDialog"
+QT_MOC_LITERAL(10, 146, 13), // "synchroDialog"
+QT_MOC_LITERAL(11, 160, 14), // "messagesDialog"
+QT_MOC_LITERAL(12, 175, 7), // "sendKey"
+QT_MOC_LITERAL(13, 183, 3), // "key"
+QT_MOC_LITERAL(14, 187, 9), // "vlmDialog"
+QT_MOC_LITERAL(15, 197, 10), // "helpDialog"
+QT_MOC_LITERAL(16, 208, 12), // "updateDialog"
+QT_MOC_LITERAL(17, 221, 11), // "aboutDialog"
+QT_MOC_LITERAL(18, 233, 14), // "gotoTimeDialog"
+QT_MOC_LITERAL(19, 248, 22), // "podcastConfigureDialog"
+QT_MOC_LITERAL(20, 271, 13), // "toolbarDialog"
+QT_MOC_LITERAL(21, 285, 12), // "pluginDialog"
+QT_MOC_LITERAL(22, 298, 9), // "epgDialog"
+QT_MOC_LITERAL(23, 308, 12), // "setPopupMenu"
+QT_MOC_LITERAL(24, 321, 16), // "destroyPopupMenu"
+QT_MOC_LITERAL(25, 338, 21), // "openFileGenericDialog"
+QT_MOC_LITERAL(26, 360, 19), // "intf_dialog_args_t*"
+QT_MOC_LITERAL(27, 380, 16), // "simpleOpenDialog"
+QT_MOC_LITERAL(28, 397, 10), // "openDialog"
+QT_MOC_LITERAL(29, 408, 14), // "openDiscDialog"
+QT_MOC_LITERAL(30, 423, 14), // "openFileDialog"
+QT_MOC_LITERAL(31, 438, 13), // "openUrlDialog"
+QT_MOC_LITERAL(32, 452, 13), // "openNetDialog"
+QT_MOC_LITERAL(33, 466, 17), // "openCaptureDialog"
+QT_MOC_LITERAL(34, 484, 14), // "PLAppendDialog"
+QT_MOC_LITERAL(35, 499, 3), // "tab"
+QT_MOC_LITERAL(36, 503, 9), // "PLOpenDir"
+QT_MOC_LITERAL(37, 513, 11), // "PLAppendDir"
+QT_MOC_LITERAL(38, 525, 15), // "streamingDialog"
+QT_MOC_LITERAL(39, 541, 8), // "QWidget*"
+QT_MOC_LITERAL(40, 550, 6), // "parent"
+QT_MOC_LITERAL(41, 557, 4), // "mrls"
+QT_MOC_LITERAL(42, 562, 8), // "b_stream"
+QT_MOC_LITERAL(43, 571, 7), // "options"
+QT_MOC_LITERAL(44, 579, 23), // "openAndStreamingDialogs"
+QT_MOC_LITERAL(45, 603, 25), // "openAndTranscodingDialogs"
+QT_MOC_LITERAL(46, 629, 13), // "openAPlaylist"
+QT_MOC_LITERAL(47, 643, 21), // "savePlayingToPlaylist"
+QT_MOC_LITERAL(48, 665, 17), // "loadSubtitlesFile"
+QT_MOC_LITERAL(49, 683, 4), // "quit"
+QT_MOC_LITERAL(50, 688, 10), // "menuAction"
+QT_MOC_LITERAL(51, 699, 16) // "menuUpdateAction"
+
+    },
+    "DialogsProvider\0toolBarConfUpdated\0\0"
+    "releaseMouseEvents\0playlistDialog\0"
+    "bookmarksDialog\0mediaInfoDialog\0"
+    "mediaCodecDialog\0prefsDialog\0"
+    "extendedDialog\0synchroDialog\0"
+    "messagesDialog\0sendKey\0key\0vlmDialog\0"
+    "helpDialog\0updateDialog\0aboutDialog\0"
+    "gotoTimeDialog\0podcastConfigureDialog\0"
+    "toolbarDialog\0pluginDialog\0epgDialog\0"
+    "setPopupMenu\0destroyPopupMenu\0"
+    "openFileGenericDialog\0intf_dialog_args_t*\0"
+    "simpleOpenDialog\0openDialog\0openDiscDialog\0"
+    "openFileDialog\0openUrlDialog\0openNetDialog\0"
+    "openCaptureDialog\0PLAppendDialog\0tab\0"
+    "PLOpenDir\0PLAppendDir\0streamingDialog\0"
+    "QWidget*\0parent\0mrls\0b_stream\0options\0"
+    "openAndStreamingDialogs\0"
+    "openAndTranscodingDialogs\0openAPlaylist\0"
+    "savePlayingToPlaylist\0loadSubtitlesFile\0"
+    "quit\0menuAction\0menuUpdateAction"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DialogsProvider[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      45,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  239,    2, 0x06 /* Public */,
+       3,    0,  240,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    0,  241,    2, 0x0a /* Public */,
+       5,    0,  242,    2, 0x0a /* Public */,
+       6,    0,  243,    2, 0x0a /* Public */,
+       7,    0,  244,    2, 0x0a /* Public */,
+       8,    0,  245,    2, 0x0a /* Public */,
+       9,    0,  246,    2, 0x0a /* Public */,
+      10,    0,  247,    2, 0x0a /* Public */,
+      11,    0,  248,    2, 0x0a /* Public */,
+      12,    1,  249,    2, 0x0a /* Public */,
+      14,    0,  252,    2, 0x0a /* Public */,
+      15,    0,  253,    2, 0x0a /* Public */,
+      16,    0,  254,    2, 0x0a /* Public */,
+      17,    0,  255,    2, 0x0a /* Public */,
+      18,    0,  256,    2, 0x0a /* Public */,
+      19,    0,  257,    2, 0x0a /* Public */,
+      20,    0,  258,    2, 0x0a /* Public */,
+      21,    0,  259,    2, 0x0a /* Public */,
+      22,    0,  260,    2, 0x0a /* Public */,
+      23,    0,  261,    2, 0x0a /* Public */,
+      24,    0,  262,    2, 0x0a /* Public */,
+      25,    1,  263,    2, 0x0a /* Public */,
+      27,    0,  266,    2, 0x0a /* Public */,
+      28,    0,  267,    2, 0x0a /* Public */,
+      29,    0,  268,    2, 0x0a /* Public */,
+      30,    0,  269,    2, 0x0a /* Public */,
+      31,    0,  270,    2, 0x0a /* Public */,
+      32,    0,  271,    2, 0x0a /* Public */,
+      33,    0,  272,    2, 0x0a /* Public */,
+      34,    1,  273,    2, 0x0a /* Public */,
+      34,    0,  276,    2, 0x2a /* Public | MethodCloned */,
+      36,    0,  277,    2, 0x0a /* Public */,
+      37,    0,  278,    2, 0x0a /* Public */,
+      38,    4,  279,    2, 0x0a /* Public */,
+      38,    3,  288,    2, 0x2a /* Public | MethodCloned */,
+      38,    2,  295,    2, 0x2a /* Public | MethodCloned */,
+      44,    0,  300,    2, 0x0a /* Public */,
+      45,    0,  301,    2, 0x0a /* Public */,
+      46,    0,  302,    2, 0x0a /* Public */,
+      47,    0,  303,    2, 0x0a /* Public */,
+      48,    0,  304,    2, 0x0a /* Public */,
+      49,    0,  305,    2, 0x0a /* Public */,
+      50,    1,  306,    2, 0x08 /* Private */,
+      51,    1,  309,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 26,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   35,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 39, QMetaType::QStringList, QMetaType::Bool, QMetaType::QStringList,   40,   41,   42,   43,
+    QMetaType::Void, 0x80000000 | 39, QMetaType::QStringList, QMetaType::Bool,   40,   41,   42,
+    QMetaType::Void, 0x80000000 | 39, QMetaType::QStringList,   40,   41,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QObjectStar,    2,
+    QMetaType::Void, QMetaType::QObjectStar,    2,
+
+       0        // eod
+};
+
+void DialogsProvider::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        DialogsProvider *_t = static_cast<DialogsProvider *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->toolBarConfUpdated(); break;
+        case 1: _t->releaseMouseEvents(); break;
+        case 2: _t->playlistDialog(); break;
+        case 3: _t->bookmarksDialog(); break;
+        case 4: _t->mediaInfoDialog(); break;
+        case 5: _t->mediaCodecDialog(); break;
+        case 6: _t->prefsDialog(); break;
+        case 7: _t->extendedDialog(); break;
+        case 8: _t->synchroDialog(); break;
+        case 9: _t->messagesDialog(); break;
+        case 10: _t->sendKey((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->vlmDialog(); break;
+        case 12: _t->helpDialog(); break;
+        case 13: _t->updateDialog(); break;
+        case 14: _t->aboutDialog(); break;
+        case 15: _t->gotoTimeDialog(); break;
+        case 16: _t->podcastConfigureDialog(); break;
+        case 17: _t->toolbarDialog(); break;
+        case 18: _t->pluginDialog(); break;
+        case 19: _t->epgDialog(); break;
+        case 20: _t->setPopupMenu(); break;
+        case 21: _t->destroyPopupMenu(); break;
+        case 22: _t->openFileGenericDialog((*reinterpret_cast< intf_dialog_args_t*(*)>(_a[1]))); break;
+        case 23: _t->simpleOpenDialog(); break;
+        case 24: _t->openDialog(); break;
+        case 25: _t->openDiscDialog(); break;
+        case 26: _t->openFileDialog(); break;
+        case 27: _t->openUrlDialog(); break;
+        case 28: _t->openNetDialog(); break;
+        case 29: _t->openCaptureDialog(); break;
+        case 30: _t->PLAppendDialog((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 31: _t->PLAppendDialog(); break;
+        case 32: _t->PLOpenDir(); break;
+        case 33: _t->PLAppendDir(); break;
+        case 34: _t->streamingDialog((*reinterpret_cast< QWidget*(*)>(_a[1])),(*reinterpret_cast< const QStringList(*)>(_a[2])),(*reinterpret_cast< bool(*)>(_a[3])),(*reinterpret_cast< QStringList(*)>(_a[4]))); break;
+        case 35: _t->streamingDialog((*reinterpret_cast< QWidget*(*)>(_a[1])),(*reinterpret_cast< const QStringList(*)>(_a[2])),(*reinterpret_cast< bool(*)>(_a[3]))); break;
+        case 36: _t->streamingDialog((*reinterpret_cast< QWidget*(*)>(_a[1])),(*reinterpret_cast< const QStringList(*)>(_a[2]))); break;
+        case 37: _t->openAndStreamingDialogs(); break;
+        case 38: _t->openAndTranscodingDialogs(); break;
+        case 39: _t->openAPlaylist(); break;
+        case 40: _t->savePlayingToPlaylist(); break;
+        case 41: _t->loadSubtitlesFile(); break;
+        case 42: _t->quit(); break;
+        case 43: _t->menuAction((*reinterpret_cast< QObject*(*)>(_a[1]))); break;
+        case 44: _t->menuUpdateAction((*reinterpret_cast< QObject*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (DialogsProvider::*_t)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&DialogsProvider::toolBarConfUpdated)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (DialogsProvider::*_t)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&DialogsProvider::releaseMouseEvents)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject DialogsProvider::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_DialogsProvider.data,
+      qt_meta_data_DialogsProvider,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *DialogsProvider::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DialogsProvider::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_DialogsProvider.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int DialogsProvider::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 45)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 45;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 45)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 45;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void DialogsProvider::toolBarConfUpdated()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void DialogsProvider::releaseMouseEvents()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
